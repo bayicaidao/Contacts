2145c7336fd2220f4f04acf7886d58984ae98e09
bayicaidao <caojie@soujiedeMacBook-Air.local>
2014-03-06 17:10:58 -0800

    通讯录 排序

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Phone
+//
+//  Created by xin dong on 10-9-6.
+//  Copyright Lixf 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
